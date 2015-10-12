d16279aeec87b18f1018b1e6230b287cbf698d29
borjabordel <borjabordel@users.noreply.github.com>
2015-10-12 19:41:54 +0200

    Create RDM8800library.ino

---
+++ CyberneticDevices/RDM8800library/RDM8800library.ino
@@ -0,0 +1,11 @@
+/*
+Librería para la lectura de etiquetas NFC desde Arduino 
+utilizando el lector RDM8800 
+http://wiki.iteadstudio.com/RDM8800
+
+@author Borja Bordel
+@date Octubre 2015
+ */
+ 
+// Librería para simular un puerto serie UART Arduino
+#include <SoftwareSerial.h>
